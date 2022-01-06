0aaadfc743734a0fcc05ac9cb6f8bb6553cb3d7f
Hassan Agmir <hagmir6@gmail.com>
2022-01-06 15:17:09 +0100

    add ads

--- src/ads/Hr.js
+++ src/ads/Hr.js
@@ -1,25 +1,26 @@
-
-const ads = `
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6043226569102012"
-            crossorigin="anonymous"></script>
-        <!-- H-content-ads -->
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-6043226569102012"
-            data-ad-slot="4567237334"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-
-`
+import React, { Component  } from 'react'
 
 
-function Hr(){
-    return(
-         <div dangerouslySetInnerHTML={{ __html: ads }} />
-    )
+class Hr extends Component {
+    componentDidMount() {
+        (window.adsbygoogle = window.adsbygoogle || []).push({})
+       }
+
+    render() {
+        return (
+                <ins className="adsbygoogle"
+                    style = { {display:"inline-block"} }
+                    data-ad-client="ca-pub-6043226569102012"
+                    data-ad-slot="4567237334"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true">
+                </ins>
+    
+        )
+
+    }
 }
+
+
 
 export default Hr;
