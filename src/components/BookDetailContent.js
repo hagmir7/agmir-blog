1ab8cd9ae9c660a5f7bbdf8530be49246af07468
Hassan Agmir <hagmir6@gmail.com>
2022-05-01 12:07:55 +0000

    Ads

--- src/components/BookDetailContent.js
+++ src/components/BookDetailContent.js
@@ -1,31 +1,18 @@
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import Hr from "../ads/Hr";
+import GoogleAd from "../ads/GoogleAd";
 
 
 const BookDetailContent = (props) => {
     return (
         <div className="container-lg">
-            <Helmet>
-            <title>{props.name}</title>
-            <meta name="description" content={props.description} ></meta>
-            <link rel='canonical' href={`/book/${props.id}`} />
-            <meta itemprop="image" content={props.image} />
-            <meta name="keywords" content={props.tags}/>
-
-            <meta property="og:url" content={`/book/${props.id}`}  />
-            <meta property="og:type" content="article" />
-            <meta property="og:title" content={props.name} />
-            <meta property="og:description" content={props.description}  />
-            <meta property="og:image" content={props.image} />
-        </Helmet>
             <div className="row" key={props.id}>
             <div className="col-12 col-md-7 col-lg-8 col-xl-8 mb-3 card m-0 shadow-sm">
                 <article className="blog-post">
                     <h1 className="blog-post-title h4 mt-2">{props.name}</h1>
                     <p className="text-left">{props.description}</p>
-                    <Hr />
-                    <hr />
+                    <GoogleAd slot="4567237334" googleAdId="pub-6043226569102012"/>
+                    <hr/>
                         <div className="d-flex justify-content-center mb-3">
                             <div className="card shadow-sm overflow-hidden">
                                 <img className="book-detail-image" src={props.image} alt={props.name} />
@@ -54,7 +41,7 @@ const BookDetailContent = (props) => {
                             </tbody>
                         </table>
                         <h2 className="h5">Download Book</h2>
-                        <Hr />
+                        <GoogleAd slot="4567237334" googleAdId="pub-6043226569102012"/>
                         <div className="w-100 my-5 d-flex justify-content-center">
                             <a className="btn btn-primary w-50" href={props.book_file} target="_blanck">DOWNLOAD</a>
                         </div>
@@ -63,7 +50,7 @@ const BookDetailContent = (props) => {
             <div className="col-12 col-md-5 col-lg-4 col-xl-4 position-relative">
                 <div className="position-sticky top-0">
                     <div className="card shadow-sm">
-                    <Hr />
+                    <GoogleAd slot="4567237334" googleAdId="pub-6043226569102012"/>
                     </div>
                     <div className="p-4 mt-3 bg-light card shadow-sm border">
                         <span className="fst-italic h4 p-1">Copyrights</span>
@@ -75,7 +62,21 @@ const BookDetailContent = (props) => {
                 </div>
             </div>
         </div>
+        <Helmet>
+            <title>{props.name}</title>
+            <meta property="og:url" content={`/book/${props.id}`}  />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={props.name} />
+            <meta property="og:description" content={props.description}  />
+            <meta property="og:image" content={props.image} />
 
+            <meta name="description" content={props.description} ></meta>
+            <link rel='canonical' href={`/book/${props.id}`} />
+            <meta itemprop="image" content={props.image} />
+            <meta name="keywords" content={props.tags}/>
+
+
+        </Helmet>
         </div>
     )
 
